b078bd7f4f133e035f34a214d7a81ca225951a25
loregramazio <lorenzogramazio9@gmail.com>
2026-03-05 16:07:22 +0100

    Add files via upload

---
+++ java/mymessage.java
@@ -0,0 +1,13 @@
+
+public class mymessage{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming".toUpperCase());
+        System.out.println("Java is a versatile language.".toLowerCase());
+        System.out.println("Java programming is fun.".length());
+        System.out.println("Java programming is fun.".charAt(5));
+        System.out.println("Java programming is fun.".substring(5, 16));
+        System.out.println("Java programming is fun.".replace("fun", "awesome"));
+        System.out.println("   Java programming is fun.   ".trim());
+    }
+}
